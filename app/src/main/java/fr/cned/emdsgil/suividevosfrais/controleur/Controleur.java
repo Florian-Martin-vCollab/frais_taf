24eeb55fe44877feb5489f57a38774f3fb917f40
Florian-Martin-vCollab <flo.rian.martin7469@gmail.com>
2021-01-19 09:10:47 +0100

    testmerge2

--- app/src/main/java/fr/cned/emdsgil/suividevosfrais/controleur/Controleur.java
+++ app/src/main/java/fr/cned/emdsgil/suividevosfrais/controleur/Controleur.java
@@ -36,7 +36,7 @@ public final class Controleur {
      * Original : Typage explicite =
      * public static Hashtable<Integer, FraisMois> listFraisMois = new Hashtable<Integer, FraisMois>();
      */
-    private int testMerge;
+    private int testMerge2;
 
     // -------- CONSTRUCTEUR --------
 
